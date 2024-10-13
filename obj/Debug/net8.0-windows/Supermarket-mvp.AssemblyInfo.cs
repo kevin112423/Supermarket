449cec58255e6f92685e1fd1e56e680ee4ccc4ae
kevingomez0527@gmail.com <kevingomez0527@gmail.com>
2024-10-12 22:11:31 -0500

    conexion a base de datos de Customers

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ea801324b3750241b2d412ed947f9e8c2bc936")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8fd5f5815d17037d2524551b3242af7341db5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
