2a12723275a0b1bd2f7e3a43e20b16328e2f1610
kevingomez0527@gmail.com <kevingomez0527@gmail.com>
2024-10-13 12:55:51 -0500

    crecion del enlanze de de bd de Product

--- Presenters/ProductPresenter.cs
+++ Presenters/ProductPresenter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Supermarket_mvp.model;
+using Supermarket_mvp.view;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,71 @@ namespace Supermarket_mvp.Presenters
 {
     internal class ProductPresenter
     {
+        private IProductView view;
+        private IProductRepository repository;
+        private BindingSource ProductBindingSource;
+        private IEnumerable<ProductModel> ProductList;
+
+        public ProductPresenter(IProductView view, IProductRepository repository)
+        {
+            this.ProductBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.AddNewEvent += AddNewCustomers;
+            this.view.SaveEvent += SaveCustomers;
+            this.view.DeleteEvent += DeleteSelectedCustomers;
+            this.view.CancelEvent += CancelCustomers;
+            this.view.EditEvent += LoadSelectCustomersToEdit;
+            this.view.SearchEvent += SearchCustomers;
+
+            this.view.SetProductListBildingSource(ProductBindingSource);
+
+            LoadAllCustomersList();
+
+            this.view.Show();
+        }
+
+        private void LoadAllCustomersList()
+        {
+            ProductList = repository.GetAll();
+            ProductBindingSource.DataSource = ProductList;
+        }
+
+        private void LoadSelectCustomersToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchCustomers(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrEmpty(view.SearchValue);
+            if (emptyValue == false)
+            {
+                ProductList = repository.GetByValue(this.view.SearchValue);
+            }
+            ProductBindingSource.DataSource = ProductList;
+        }
+
+        private void DeleteSelectedCustomers(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CancelCustomers(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveCustomers(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewCustomers(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ _repository/ProductRepository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+using Supermarket_mvp.model
+
+namespace Supermarket_mvp._repository
+{
+    internal class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(string connectionString)
+        {
+            this.connectString = connectionString;
+        }
+
+        public void Add(ProductModel productModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ProductModel productModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(ProductModel productModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductModel> GetAll()
+        {
+            var productModelList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectString))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Product ORDER BY Product_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["Product_Id"];
+                        productModel.Name = reader["Product_Name"].ToString();
+                        productModel.Price = (decimal)reader["Product_Price"];
+                        productModel.Stock = (int)reader["Product_Stock"];
+                        productModelList.Add(productModel);
+                    }
+                }
+            }
+            return productModelList;
+
+        }
+
+        public IEnumerable<ProductModel> GetByValue(string value)
+        {
+            var productModeList = new List<ProductModel>();
+            int productId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string productName = value;
+            using (var connection = new SqlConnection(connectString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Product
+                                        WHERE Product_Id=@id or Product_Name LIKE @name+ '%'
+                                        ORDER By Product_Id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["Product_Id"];
+                        productModel.Name = reader["Product_Name"].ToString();
+                        productModel.Price = (decimal)reader["Product_Price"];
+                        productModel.Stock = (int)reader["Product_Stock"];
+                        productModeList.Add(productModel);
+
+                    }
+                }
+            }
+            return productModeList;
+        }
+
+    }
+}
+}
